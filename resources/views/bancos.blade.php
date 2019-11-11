febc20ac61de5f0e3be472f581188235e9fbbdbe
Hewlett Packard <Hewlett Packard@JR95>
2019-11-10 23:47:41 -0600

    Se integraron campos e iconos al formulario de bancos

--- resources/views/bancos.blade.php
+++ resources/views/bancos.blade.php
@@ -1,13 +1,43 @@
 @extends("plantilla")
-@section("modulo")Bancos @endsection
+@section("modulo")<div><i class="fas fa-piggy-bank"></i> Bancos</div> @endsection
 @section("contenido")
 <div class="row">
     <div class="col-md-3">
         <div class="input-group mb-3">
           <div class="input-group-prepend">
-           <span class="input-group-text" id="basic-addon1">@</span>
+           <span class="input-group-text" id="basic-addon1"><i class="fas fa-at"></i></i></span>
          </div>
-       <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1">
+       <input type="text" class="form-control" placeholder="ID" aria-label="Username" aria-describedby="basic-addon1">
+    </div>
+    </div>
+</div>
+<div class="row">
+    <div class="col-md-3">
+        <div class="input-group mb-3">
+          <div class="input-group-prepend">
+           <span class="input-group-text" id="basic-addon1"><i class="fas fa-university"></i></span>
+         </div>
+       <input type="text" class="form-control" placeholder="Nombre del Banco" aria-label="Username" aria-describedby="basic-addon1">
+    </div>
+    </div>
+</div>
+<div class="row">
+    <div class="col-md-3">
+        <div class="input-group mb-3">
+          <div class="input-group-prepend">
+           <span class="input-group-text" id="basic-addon1"><i class="fas fa-phone-square-alt"></i></span>
+         </div>
+       <input type="text" class="form-control" placeholder="Telefono" aria-label="Username" aria-describedby="basic-addon1">
+    </div>
+    </div>
+</div>
+<div class="row">
+    <div class="col-md-3">
+        <div class="input-group mb-3">
+          <div class="input-group-prepend">
+           <span class="input-group-text" id="basic-addon1"><i class="fas fa-barcode"></i></span>
+         </div>
+       <input type="text" class="form-control" placeholder="Codigo" aria-label="Username" aria-describedby="basic-addon1">
     </div>
     </div>
 </div>
